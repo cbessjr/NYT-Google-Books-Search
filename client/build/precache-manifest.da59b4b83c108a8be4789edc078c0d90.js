00393e2204dfbad7aa20366b53c7f35f5128f387
Ender <cbessjr@gmail.com>
2019-03-12 20:51:19 -0700

    push of activity that was working but is no longer working after assistance. Researching the cause

---
+++ client/build/precache-manifest.da59b4b83c108a8be4789edc078c0d90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c23f4696f2138bb4fa74",
+    "url": "/static/js/main.186db839.chunk.js"
+  },
+  {
+    "revision": "4f1fe6162a83dee29630",
+    "url": "/static/js/2.d0405335.chunk.js"
+  },
+  {
+    "revision": "c23f4696f2138bb4fa74",
+    "url": "/static/css/main.170d6a9b.chunk.css"
+  },
+  {
+    "revision": "8e0045f07dc2b60c3e21d2b52d953699",
+    "url": "/index.html"
+  }
+];
---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const bookRoutes = require("./books");
+
+// Book routes
+// /api/books
+router.use("/books", bookRoutes);
+
+module.exports = router;
